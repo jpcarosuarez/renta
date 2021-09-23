38b050dbfa8263489e280cad467130ab3d5edd7d
jpcarosuarez <46928724+jpcarosuarez@users.noreply.github.com>
2021-09-22 22:11:29 -0500

    inicio

---
+++ src/components/Main/Main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from '../Navbar/Navbar';
+
+// import Footer from './Footer/Footer';
+
+function Main() {
+    return (
+        <div>
+            <Navbar />
+
+        </div>
+    )
+}
+
+export default Main;
